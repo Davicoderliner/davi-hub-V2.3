8b1008253e9ccb3ca0409181caf46cf76691b87a
daviscriptsOFFICIAL <112778327+Davicoderliner@users.noreply.github.com>
2023-03-23 21:10:13 -0300

    Create Maindavi.lua

---
+++ Maindavi.lua
@@ -0,0 +1,36 @@
+local VLib = loadstring(game:HttpGet("https://pastebin.com/raw/Mb49kKTP"))()
+
+
+		MAINTTL = "DAVI HUB V1 com 100 scripts" 
+
+local win = VLib:Window("davi hub", Color3.fromRGB(196, 40, 28))
+
+local ss1 = win:Tab("HOW TO USE")
+local ss = win:Tab("MAIN")
+local sss = win:Tab("MISC")
+local cred = win:Tab("CREDITS")
+
+ss1:Button("Destroy Gui",function()
+game.CoreGui["Library"]:Destroy()
+end)
+
+ss:Button("gui para jogos",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TakeModzz/Games-Hub-Script/main/Games%20Hub%20(Always%20updated)"))()
+end)
+   
+ ss:Button("prison life fe weapon",function()
+loadstring(game:HttpGet("https://pastebin.com/raw/AZVi2tuK"))()
+end)
+
+ss:Button("Arceus x ",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/Arceus%20X%20V3"))()
+end)
+
+ss:Button("ghost hub com 100 scripts",function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/GhostHub'))()
+
+end)
+
+cred:Label("MADE BY davi.scripts")
+cred:Label("se quiser fazer video do meu script me marque que eu te coloco na proxima atualização")
+cred:Label(" eu :), obrigada por testa o script  ")
