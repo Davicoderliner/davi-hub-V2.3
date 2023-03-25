a23d230cc3de0c813460f46f13b7d8c884da138d
daviscriptsOFFICIAL <112778327+Davicoderliner@users.noreply.github.com>
2023-03-25 15:30:32 -0300

    Update Daviguiv2loader.lua

--- Daviguiv2loader.lua
+++ Daviguiv2loader.lua
@@ -1,4 +1,51 @@
+-- Create a ScreenGui to hold the credits
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "Credits"
+ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
 
+-- Create an ImageLabel to display the image
+local ImageLabel = Instance.new("ImageLabel")
+ImageLabel.Name = "Image"
+ImageLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+ImageLabel.Position = UDim2.new(0.5, 0, 0.4, 0)
+ImageLabel.Size = UDim2.new(0.2, 0, 0.2, 0)
+ImageLabel.Image = "https://www.roblox.com/asset-thumbnail/image?assetId=1117607358&thumbnailFormat=png&width=420&height=420&fit=bounds"
+ImageLabel.Parent = ScreenGui
+
+-- Create a TextLabel to display the text
+local TextLabel = Instance.new("TextLabel")
+TextLabel.Name = "Text"
+TextLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+TextLabel.Position = UDim2.new(0.5, 0, 0.6, 0)
+TextLabel.Size = UDim2.new(0.5, 0, 0.1, 0)
+TextLabel.Font = Enum.Font.GothamBold
+TextLabel.TextColor3 = Color3.new(1, 1, 1)
+TextLabel.TextScaled = true
+TextLabel.Text = "made by daviscripts"
+TextLabel.Parent = ScreenGui
+
+-- Create a function to animate the credits
+local function animateCredits()
+    -- Move the ImageLabel and TextLabel up
+    local TweenService = game:GetService("TweenService")
+    local imageTween = TweenService:Create(ImageLabel, TweenInfo.new(10, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {Position = UDim2.new(0.5, 0, -0.2, 0)})
+    local textTween = TweenService:Create(TextLabel, TweenInfo.new(10, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {Position = UDim2.new(0.5, 0, -0.1, 0)})
+    imageTween:Play()
+    textTween:Play()
+    
+    -- Wait for the animation to finish
+    wait(10)
+    
+    -- Reset the ImageLabel and TextLabel positions
+    ImageLabel.Position = UDim2.new(0.5, 0, 0.4, 0)
+    TextLabel.Position = UDim2.new(0.5, 0, 0.6, 0)
+    
+    -- Remove the ScreenGui
+    ScreenGui:Destroy()
+end
+
+-- Call the function to start the animation
+animateCredits()
 
 -- Define the credit text and duration
 local creditText = "Made by DAVISCRIPT"
